c9ade9a6d6a7ff6cbb34eb39707edee055760d65
KarenSuprema123 <110925343+KarenSuprema123@users.noreply.github.com>
2023-06-13 02:32:44 -0600

    Proyecto completo

--- modulos/inicio.php
+++ modulos/inicio.php
@@ -10,7 +10,46 @@
     <div class="col">
         <div class="card mt-3">
             <div class="card-body">
-           Este es el inicio
+           <section class="carrusel">
+           <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://picsum.photos/id/25/1920/500" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h2 style="color: #000000;">♥ Bienvenidos a nuestro sitio web </h2>
+        <p style="color: #000000;">!!!Comienza a agendar ya</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://picsum.photos/id/20/1920/500" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h2 style="color: #000000;">Organiza tus eventos de manera diferente</h2>
+        <p></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://picsum.photos/id/42/1920/500" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h2 style="color: #000000;">Comienza yaaaaa, es GRATIS</h2>
+        <p style="color: #000000;">Es muy facil de usar</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div> 
+           </section>
         </div>
      </div>
     </div>
